add6750d1118af3539b134b4b93d6fc019ba950d
WlanWerner <38541809+WlanWerner@users.noreply.github.com>
2019-01-30 18:16:45 +0100

    Add favicon

--- templates/basic.template.php
+++ templates/basic.template.php
@@ -4,8 +4,8 @@
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<!-- favicon -->
-		<link rel="shortcut icon" href="img/{{COINNAME}}.ico">
+	<!-- favicon -->
+	<link rel="shortcut icon" href="img/favicon.ico">
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
         <!-- FontAwesome -->
